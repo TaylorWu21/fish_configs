050475c977204bc966f523a68f1cf1f08523fc53
Taylor Wu <taylor.wu@podium.com>
2021-05-11 16:07:14 -0600

    update fish

--- config.fish
+++ config.fish
@@ -1,3 +1,4 @@
 source ~/.asdf/asdf.fish
 source ~/.fish_variables
 set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
+set -g  fish_user_paths "/usr/local/opt/openssl" $fish_user_paths
