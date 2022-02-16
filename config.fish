e5e93fbd1c4c084e31390b9ed50c4fa836fe6505
Taylor Wu <taylor.wu@podium.com>
2022-02-16 14:42:33 -0700

    update config file

--- config.fish
+++ config.fish
@@ -1,9 +1,10 @@
 # Imports
 source ~/.asdf/asdf.fish
 source ~/.fish_variables
+source ~/.bashrc
 
 # Path
 set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
-set -g  fish_user_paths "/usr/local/opt/openssl" $fish_user_pathsset -g fish_user_paths "/usr/local/opt/m4/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openssl" $fish_user_pathsset -g fish_user_paths "/usr/local/opt/m4/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/openssl@1.1/bin" $fish_user_paths
 set -g GARDEN_GITLAB_TOKEN "NXhg4igfzpwmM3wSrvvS"
